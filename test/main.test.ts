c4e3b41de7c84faff89d78c8f2689b12a1ca710b
Neil Guan <guan840912@gmail.com>
2021-01-03 19:16:52 +0800

    update testing

--- test/main.test.ts
+++ test/main.test.ts
@@ -7,5 +7,4 @@ test('Snapshot', () => {
   const stack = new MyStack(app, 'test');
 
   expect(stack).not.toHaveResource('AWS::S3::Bucket');
-  expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
 });
